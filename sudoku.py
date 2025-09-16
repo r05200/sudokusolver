4054ba9919b983130648aab97906ff2b2a529c5c
r05200 <jeremyniu0520@gmail.com>
2025-09-15 22:07:02 -0400

    Row col and box checks
    implemented checking numbers in box, row and column to find possible values

--- sudoku.py
+++ sudoku.py
@@ -1,32 +1,43 @@
 import math
+import sys
 def main():
-    
+    sortedUnknowns = []
+    unknown = {}
     while True:
         try:
             #get side length, until enters a valid value
             sideL = int(input("Side length: "))
+            
             break
         except ValueError:
             pass
     startGrid, unknownList = startingGrid(sideL)
-    for row in startGrid:
+    
+    print(unknownList)
+    for pos in unknownList:
+    
+        guessList = list(range(1,sideL+1))
+        p = checkCol(startGrid, pos[1], sideL, guessList)
+        t = checkRow(startGrid, pos[0], sideL, p)
+        z = checkBox(startGrid, pos, sideL, t)
+        print(z)
+        sortedUnknowns.append([pos, z])
 
-        print(row)
-    for row in unknownList:
-        print(row)
+
 
 def startingGrid(l):
     grid = []
     unknown = []
+
     #iterates through each number, so each row out of the total
     for i in range(l):
         while True:
             inputRow = input("Input row: ").split(" ")
-        
             if len(inputRow) == l:
                 a = 0
                 for e in inputRow:
                     if e.isalpha():
+                        #appends positions of all unknowns to unknownlist
                         unknown.append([i,inputRow.index(e, a)])
                     if not e.isalnum:
                         break
@@ -34,32 +45,47 @@ def startingGrid(l):
                 break
             else:
                 print("Row is not equivalent to side length")
-        
+    #puts row in list, to form the starting grid
         grid.append(inputRow)
     return grid, unknown
 
-def guessRow(grid, row, length):
+def checkRow(grid, row, sideLen, possible):
     #removes values that are in the row, by iterating through each column
-    for col in length:
+    for col in list(range(sideLen)):
         try:
-            length.remove(grid[row][col])
+            possible.remove(int(grid[row][col]))
+            #print("removed" + str(grid[row][col]))
         except ValueError:
             pass
-    return length
+    return possible
 
-def guessCol(grid, col, length):
+def checkCol(grid, col, sideLen, possible):
     #removes values that are in the column, by iterating through each row
-    for row in length:
+    for row in list(range(sideLen)):
         try:
-            length.remove(grid[row][col])
+            possible.remove(int(grid[row][col]))
+            #print("removed" + str(grid[row][col]))
         except ValueError:
             pass
-    return length
+    return possible
 
-def guessGrid(grid, position, length):
-    gridLength = math.sqrt(len(length))
-    (position[0]+1)%gridLength
-
+def checkBox(grid, position, sideLen, possible):
+    boxLength = math.sqrt(sideLen)
+    boxLeftBound = int(position[1] - (int(position[1]))%boxLength)
+    boxUpperBound = int(position[0] - int(position[0])%boxLength)
+    for entry in range(boxUpperBound, int(boxUpperBound + boxLength)):
+        for entry2 in range(boxLeftBound, int(boxLeftBound + boxLength)):
+            try:
+                possible.remove(int(grid[entry][entry2]))
+            except ValueError:
+                pass
+    return possible
 
 
 main()
+
+#test sudoku 2x2
+# 2 4 3 1       x 4 t 1    expected: (2, 4), (2, 3), (2, 3), (2, 3)
+# 3 1 4 2       3 1 4 d    expected: (2), (2)
+# 1 3 2 4       1 c v 4    expected: (2, 3), (3), (2, 3), (2, 3)
+# 4 2 1 3       x 2 1 3    expected: (2, 4), (4)
